9e2898e67482c28184b95c8de1e00fbf66289d21
text.c <2982748088@qq.com>
2021-02-05 11:55:08 +0800

    二分查找

--- text.c
+++ text.c
@@ -1,23 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<math.h>
+int binary_search(int arr[], int k, int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return -1;
+}
 int main()
 {
-	int n = 0;
-	while (~scanf("%d", &n))
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };//有序数组
+	int sz = sizeof(arr) / sizeof(arr[0]);//计算数组的长度
+	int k = 0;
+	scanf("%d", &k);//输入要查找的值
+	int ret = binary_search(arr, k, sz);
+	if (ret == -1)
 	{
-		int m = 0;
-		scanf("%d", &m);
-		int i = 0;
-		double temp = (double)n;
-		double sum = (double)n;
-		for (i = 1; i < m; i++)
-		{
-			temp = sqrt(temp);
-			sum += temp;
-		}
-		printf("%.2lf\n", sum);
+		printf("没找到!\n");
+	}
+	else
+	{
+		printf("找到了!下标是：%d\n", ret);
 	}
 	return 0;
-
 }
